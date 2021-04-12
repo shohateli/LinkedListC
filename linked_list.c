f26f27b7c7b14fb7cdb9a2e3703065cadfb14f89
Eli <eashohat@gmail.com>
2021-04-12 15:29:32 +0300

    need to use header files

--- linked_list.c
+++ linked_list.c
@@ -7,17 +7,6 @@ struct link
     struct link *next;
 };
 
-void print_list(struct link link)
-{
-    printf("%d ->", link.data);
-    while (link.next != NULL)
-    {
-        link = *link.next;
-        printf("%d ->", link.data);
-    }
-    printf("NULL");
-}
-
 int get_len_of_list(struct link list)
 {
     int len = 1;
@@ -27,6 +16,25 @@ int get_len_of_list(struct link list)
         len++;
     }
     return len;
+}
+
+void print_list(struct link *list)
+{
+    if (list == NULL)
+    {
+        printf("empty");
+    }
+    else
+    {
+        struct link listModel = *list;
+        printf("%d ->", listModel.data);
+        while (listModel.next != NULL)
+        {
+            listModel = *listModel.next;
+            printf("%d ->", listModel.data);
+        }
+        printf("NULL\n");
+    }
 }
 
 void freeList(struct link *list)
@@ -50,6 +58,21 @@ struct link *get_last_item(struct link *list)
         return (NULL);
     }
     while (currentElement->next != NULL)
+    {
+        currentElement = currentElement->next;
+    }
+    return (currentElement);
+}
+
+struct link *get_element_before_x(struct link *list, struct link *before)
+{
+    struct link *currentElement;
+    currentElement = list;
+    if (currentElement == NULL)
+    {
+        return (NULL);
+    }
+    while (currentElement->next != before)
     {
         currentElement = currentElement->next;
     }
@@ -81,24 +104,112 @@ struct link *create(int data, struct link **list, int *isFirst)
     }
 }
 
+struct link *add_new_to_start(struct link **list, int data)
+{
+    // struct link *temp = *list;
+    struct link *placeForNewData = malloc(sizeof(struct link));
+    placeForNewData->data = data;
+    placeForNewData->next = *list;
+    *list = placeForNewData;
+    return placeForNewData;
+    // printf("%d", (*list)->data);
+}
+
+void move_to_start(struct link **list, struct link *dataToMove)
+{
+    // struct link *temp = *list;
+    struct link *before = get_element_before_x(*list, dataToMove);
+    before->next = dataToMove->next;
+    dataToMove->next = *list;
+    *list = dataToMove;
+}
+
+void free_last_item(struct link *list)
+{
+    struct link *last = get_last_item(list);
+    struct link *before = get_element_before_x(list, last);
+    before->next = NULL;
+    free(last);
+}
+
+struct link *getItemByIndex(struct link *list, int index)
+{
+    struct link *currentElement;
+    currentElement = list;
+    int count = 0;
+    if (currentElement == NULL)
+    {
+        return (NULL);
+    }
+    while (count != index)
+    {
+        currentElement = currentElement->next;
+        count++;
+    }
+    return (currentElement);
+}
+
+void free_by_index(struct link **list, int index, int *isFirst)
+{
+    struct link *byIndex = getItemByIndex(*list, index);
+    struct link *lastElement = get_last_item(*list);
+    if (byIndex == lastElement && index == 0)
+    {
+        *isFirst = 1;
+        free(byIndex);
+        *list = NULL;
+    }
+    else if (index == 0)
+    {
+        *list = byIndex->next;
+        free(byIndex);
+    }
+    else if (byIndex == lastElement)
+    {
+        struct link *before = get_element_before_x(*list, byIndex);
+        before->next = NULL;
+        free(byIndex);
+    }
+    else
+    {
+        struct link *before = get_element_before_x(*list, byIndex);
+        before->next = byIndex->next;
+        free(byIndex);
+    }
+}
+
+void sortListSmallToBig(struct link *list)
+{
+    struct link *i = list;
+    struct link *j = list->next;
+    for (i = list; i != NULL; i = i->next)
+    {
+        for (j = i->next; j != NULL; j = j->next)
+        {
+            if (i->data > j->data)
+            {
+                int temp = i->data;
+                i->data = j->data;
+                j->data = temp;
+            }
+        }
+    }
+}
+
 int main()
 {
     struct link *list = NULL;
     int isFirst = 1;
 
-    // struct link *new = create(2, &link1, &link1, 1);
-    // print_list(*list);
-    create(5, &list, &isFirst);
-    create(4, &list, &isFirst);
-    create(3, &list, &isFirst);
-    create(2, &list, &isFirst);
-    create(1, &list, &isFirst);
+    struct link *first = create(5, &list, &isFirst);
+    struct link *second = create(4, &list, &isFirst);
+    struct link *third = create(3, &list, &isFirst);
+    struct link *fourth = create(2, &list, &isFirst);
+    struct link *fifth = create(1, &list, &isFirst);
+    print_list(list);
+    sortListSmallToBig(list);
+    print_list(list);
 
-    // printf("%d\n", get_last_item(list)->data);
-    print_list(*list);
-    // printf("%d", last.data);
-    // printf("%d", get_len_of_list(*list));
-    // free(new);
     freeList(list);
 
     return 0;
